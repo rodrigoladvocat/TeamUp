3e86a9327e537078296cc06255f6c64d888f30cc
rrlc <ladvocatrodrigo@gmail.com>
2024-06-17 17:00:10 -0300

    fixed the initialDate and finalDate comparison in cycle.service.ts + fixed a couple of validators in create-user.dto.ts + fixed @Get route in user.controller.ts

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,74 +1,95 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsString, IsBoolean, MinLength, IsNumber, IsPhoneNumber, Length, IsPostalCode } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsBoolean,
+  MinLength,
+  IsNumber,
+  IsPhoneNumber,
+  Length,
+  IsPostalCode,
+} from 'class-validator';
 
 export class CreateUserDto {
-    @ApiProperty({ description: 'Username.', example: 'Fulano da Silva' })
-    @IsNotEmpty()
-    @IsEmail()
-    @IsString()
-    name: string
+  @ApiProperty({ description: 'Username.', example: 'Fulano da Silva' })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
 
-    @ApiProperty({ description: 'email', example: 'fulano@gmail.com' })
-    @IsNotEmpty()
-    @IsEmail()
-    email: string
+  @ApiProperty({ description: 'email', example: 'fulano@gmail.com' })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 
-    @ApiProperty({ description: 'User pass with length >= 8', example: '12345678' })
-    @IsNotEmpty()
-    @MinLength(8)
-    password: string
+  @ApiProperty({
+    description: 'User pass with length >= 8',
+    example: '12345678',
+  })
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
 
-    @ApiProperty({ description: 'URL of user profile image.', example: 'TODO - put default image' })
-    @IsNotEmpty()
-    imgUrl: string
+  @ApiProperty({
+    description: 'URL of user profile image.',
+    example: 'TODO - put default image',
+  })
+  @IsNotEmpty()
+  imgUrl: string;
 
-    @ApiProperty({ description: 'Boolean flag. false means isCollaborator.', example: true })
-    @IsNotEmpty()
-    @IsBoolean()
-    isManager: boolean
+  @ApiProperty({
+    description: 'Boolean flag. false means isCollaborator.',
+    example: true,
+  })
+  @IsNotEmpty()
+  @IsBoolean()
+  isManager: boolean;
 
-    @ApiProperty({ description: 'User role.', example: 'Sócio' })
-    @IsNotEmpty()
-    @IsString()
-    role: string
+  @ApiProperty({ description: 'User role.', example: 'Sócio' })
+  @IsNotEmpty()
+  @IsString()
+  role: string;
 
-    @ApiProperty({ description: 'User age', example: 20 })
-    @IsNotEmpty()
-    @IsNumber()
-    age: number
+  @ApiProperty({ description: 'User age', example: 20 })
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
 
-    @ApiProperty({ description: 'User phone', example: '08191234-1234' })
-    @IsNotEmpty()
-    @IsPhoneNumber('BR')
-    telephone: string
+  @ApiProperty({ description: 'User phone', example: '08191234-1234' })
+  @IsNotEmpty()
+  @IsPhoneNumber('BR')
+  telephone: string;
 
-    @ApiProperty({ description: 'User cpf', example: 'Some name' })
-    @IsNotEmpty()
-    @Length(11, 11)
-    cpf: string
+  @ApiProperty({ description: 'User cpf', example: 'Some name' })
+  @IsNotEmpty()
+  @Length(11, 11)
+  cpf: string;
 
-    @ApiProperty({ description: 'User street address', example: 'Rua Inexistente' })
-    @IsNotEmpty()
-    @IsString()
-    street: string
+  @ApiProperty({
+    description: 'User street address',
+    example: 'Rua Inexistente',
+  })
+  @IsNotEmpty()
+  @IsString()
+  street: string;
 
-    @ApiProperty({ description: 'User street number address', example: '28' })
-    @IsNotEmpty()
-    @IsString()
-    number: string
+  @ApiProperty({ description: 'User street number address', example: '28' })
+  @IsNotEmpty()
+  @IsString()
+  number: string;
 
-    @ApiProperty({ description: 'User city address', example: 'Recife' })
-    @IsNotEmpty()
-    @IsString()
-    city: string
+  @ApiProperty({ description: 'User city address', example: 'Recife' })
+  @IsNotEmpty()
+  @IsString()
+  city: string;
 
-    @ApiProperty({ description: 'User state address', example: 'Brasil' })
-    @IsNotEmpty()
-    @IsString()
-    state: string
+  @ApiProperty({ description: 'User state address', example: 'Brasil' })
+  @IsNotEmpty()
+  @IsString()
+  state: string;
 
-    @ApiProperty({ description: 'User zipCode address', example: '12123-123' })
-    @IsNotEmpty()
-    @IsPostalCode('BR')
-    zipCode: string
+  @ApiProperty({ description: 'User zipCode address', example: '12123-123' })
+  @IsNotEmpty()
+  @IsPostalCode('BR')
+  zipCode: string;
 }
