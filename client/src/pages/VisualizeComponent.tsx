84ff768409b1573158a8439341d9f48f83ba4960
rrlc <ladvocatrodrigo@gmail.com>
2024-06-20 15:35:23 -0300

    card component done

--- client/src/pages/VisualizeComponent.tsx
+++ client/src/pages/VisualizeComponent.tsx
@@ -1,10 +1,13 @@
+import Card from "../components/Card";
+import img1 from "../assets/img1.svg";
+
 interface Props {}
 
 export default function VisualizeComponent({}: Props): JSX.Element {
 
   return (
     <div className="snap-center self-center items-center origin-center place-self-center justify-self-center">
-      {/* Coloque o componente aqui para visualizar */}
+      <Card imageSrc={img1} email="alguem@gmail.com" name="Alguém da Silva" role="Software Developer"></Card>
 
     </div>
   );
