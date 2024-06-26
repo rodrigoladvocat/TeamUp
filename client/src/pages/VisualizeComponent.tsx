821eeb7d6a5a18375fee64dd91bc622519e7040a
VictorG-028 <victor6g0@gmail.com>
2024-06-25 21:06:37 -0300

    Initialize Shadcn UI lib

--- client/src/pages/VisualizeComponent.tsx
+++ client/src/pages/VisualizeComponent.tsx
@@ -1,14 +1,32 @@
-import Card from "../components/Card";
-import img1 from "../assets/img1.svg";
+import { Button } from "@/components/ui/button";
+import { TooltipProvider, TooltipTrigger, TooltipContent, Tooltip } from "@radix-ui/react-tooltip";
+import {InfoIcon} from 'lucide-react';
 
 interface Props {}
+
+function TooltipDemo() {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          {/* Estiliza e/ou adiciona HTML daqui dentro pra ficar de acordo com figma */}
+            {/* <Button variant="outline" className="">Hover</Button> */}
+            <InfoIcon className="stroke-primary"></InfoIcon>
+        </TooltipTrigger>
+        <TooltipContent>
+          {/* Estiliza e/ou adiciona HTML daqui dentro pra ficar de acordo com figma */}
+          <p className="bg-white text-primary rounded-sm px-2 py-1 mb-3">Biblioteca de UI adicionada</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
 
 export default function VisualizeComponent({}: Props): JSX.Element {
 
   return (
     <div className="snap-center self-center items-center origin-center place-self-center justify-self-center">
-      <Card imageSrc={img1} email="alguem@gmail.com" name="Alguém da Silva" role="Software Developer"></Card>
-
+      {TooltipDemo()}
     </div>
   );
 };
