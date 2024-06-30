13851236a45daf49fefc1bc04a0cff8557f5521a
VictorG-028 <victor6g0@gmail.com>
2024-06-30 12:31:36 -0300

    change schema.prisma + Update seeding + add users with photos

--- server/prisma/seed/createMockTunings.ts
+++ server/prisma/seed/createMockTunings.ts
@@ -1,19 +1,43 @@
-// import { Tuning } from "@prisma/client";
+import { Tuning } from "@prisma/client";
 
-// export const tunings: Tuning[] = [
-//   {
-//     collaboratorUserId: 1,
-//     managerUserId: 2,
-//     cycleId: 1,
-//     id: 1,
-//     grade: 3
-//   },
-//   {
-//     collaboratorUserId: 2,
-//     managerUserId: 1,
-//     cycleId: 1,
-//     id: 2,
-//     grade: 3
-//   }
-//   // Adicione mais afinações conforme necessário
-// ];
+export const tunings: Tuning[] = [
+  {
+    id: 1,
+    collaboratorUserId: 1,
+    managerUserId: 7,
+    cycleId: 1,
+
+    grade: (5 + 4 + 3 + 5 + 4 + 5 + 4 + 3 + 4) / 9,
+    ownershipMentalityGrade: 5,
+    learningAgilityGrade: 4,
+    resilienceAdversityGrade: 3,
+    teamworkGrade: 5,
+    outOfTheBoxThinkingBehavioralGrade: 4,
+    deliveringQualityGrade: 5,
+    meetingDeadlinesGrade: 4,
+    doingMoreWithLessGrade: 3,
+    outOfTheBoxThinkingExecutionGrade: 4,
+
+    lastUpdated: new Date(),
+  },
+  {
+    id: 2,
+    collaboratorUserId: 2,
+    managerUserId: 7,
+    cycleId: 1,
+
+    grade: (5 + 2 + 3 + 2 + 4 + 5 + 4 + 3 + 4) / 9,
+    ownershipMentalityGrade: 2,
+    learningAgilityGrade: 4,
+    resilienceAdversityGrade: 2,
+    teamworkGrade: 5,
+    outOfTheBoxThinkingBehavioralGrade: 4,
+    deliveringQualityGrade: 5,
+    meetingDeadlinesGrade: 4,
+    doingMoreWithLessGrade: 3,
+    outOfTheBoxThinkingExecutionGrade: 4,
+
+    lastUpdated: new Date(),
+  },
+  // Adicione mais afinações conforme necessário
+];
