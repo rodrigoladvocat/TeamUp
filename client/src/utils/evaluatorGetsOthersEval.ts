1f707fc3958540b1dc1da18186502aace0d4d7fd
rrlc <ladvocatrodrigo@gmail.com>
2024-07-01 03:45:57 -0300

    progress bar integrated + a couple of bugs fixed inside the progress bar component

--- client/src/utils/evaluatorGetsOthersEval.ts
+++ client/src/utils/evaluatorGetsOthersEval.ts
@@ -3,7 +3,6 @@ import { api } from "@/services/apiService";
 export async function evaluatorGetsOthersEval(userId: number, cycleId: number) {
     try{
         const response = await api.get("/others-evaluation/others/evaluator-get/" + userId + "/" + cycleId);
-        console.log(response.data);
         return response.data;
     }
     catch (error) {
