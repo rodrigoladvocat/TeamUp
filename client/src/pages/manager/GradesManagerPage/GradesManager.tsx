7326c1b23a1f467e65c29fabe5aaa116c6765ffd
Vinicius Pereira <vpa2@cin.ufpe.br>
2024-06-30 22:20:19 -0300

    card and grades manager

--- client/src/pages/manager/GradesManagerPage/GradesManager.tsx
+++ client/src/pages/manager/GradesManagerPage/GradesManager.tsx
@@ -5,77 +5,82 @@ import { useEffect, useState } from "react";
 import { getCollaboratorsByName } from "@/utils/getCollaboratorsByName";
 import Card from "@/components/Card";
 import { SearchBarProvider, useSearchBar } from "@/context/SearchBarContext";
-import { useAuth } from "@/hooks/AuthUser";
+import { Link } from "react-router-dom";
 
 interface CollaboratorProps {
-    name: string;
-    role: string;
-    email: string;
-    imageSrc: string;
+  name: string;
+  role: string;
+  email: string;
+  imageSrc: string;
+  id: number;
 }
 
 const About = () => {
+  const [collaborators, setCollaborators] = useState<CollaboratorProps[]>([]);
+  const { search } = useSearchBar();
 
-    const [ collaborators, setCollaborators ] = useState<CollaboratorProps[]>([]);
-    const { search } =  useSearchBar();
+  useEffect(() => {
+    let append_array: CollaboratorProps[] = [];
+    let param = !search ? " " : search;
 
-    useEffect(() => {
-        let append_array: CollaboratorProps[] = [];
-        let param = (!search ? " " : search);
-
-        getCollaboratorsByName(param)
-        .then((data) => {
-            data.forEach((collaborator: any) => {
-
-                append_array.push({
-                    name: collaborator.name,
-                    role: collaborator.role,
-                    email: collaborator.email,
-                    imageSrc: collaborator.imgUrl
-                });
-
-            });
-
-            setCollaborators(append_array);
-
+    getCollaboratorsByName(param).then((data) => {
+      data.forEach((collaborator: any) => {
+        append_array.push({
+          name: collaborator.name,
+          role: collaborator.role,
+          email: collaborator.email,
+          imageSrc: collaborator.imgUrl,
+          id: collaborator.id,
         });
+      });
 
-    }, [search]);
+      setCollaborators(append_array);
+    });
+  }, [search]);
 
-    return (
-        <div className="flex flex-1 p-6 min-h-screen bg-gray-900 text-white">
-            <div className="flex flex-1">
-            <aside>
-                <div>
-                <Menu></Menu>
-                </div>
-            </aside>
+  return (
+    <div className="flex flex-1 p-6 min-h-screen bg-gray-900 text-white">
+      <div className="flex flex-1">
+        <aside>
+          <div>
+            <Menu></Menu>
+          </div>
+        </aside>
 
-            <main className="flex-1 p-6 bg-general-background">
-                
-                <Header userName="Pedro Almeida" subtitle="Selecione o colaborador que deseja visualizar informações sobre" profileImage="/profile.jpg" title="Sobre a Plataforma"/>
-                
-                <SearchBar/>
+        <main className="flex-1 p-6 bg-general-background">
+          <Header
+            userName="Pedro Almeida"
+            subtitle="Selecione o colaborador que deseja visualizar informações sobre"
+            profileImage="/profile.jpg"
+            title="Sobre a Plataforma"
+          />
 
-                <div className="flex flex-1 bg-content-background h-[750px] rounded-xl mt-4 overflow-y-auto">
-                <div className="flex flex-wrap flex-1 justify-center gap-x-[52px] pt-10 pb-8">
-                        {
-                            collaborators.map(( collaborator ) => {
-                                return (
-                                    <div key={collaborator.email} className="w-[256px] h-[310px]">
-                                        <Card imageSrc={ collaborator.imageSrc } 
-                                            name={ collaborator.name } 
-                                            role={ collaborator.role } 
-                                            email={ collaborator.email }/>
-                                    </div>
-                                );
-                            })
-                        }
-                </div>
-                </div>
-            </main>
+          <SearchBar />
+
+          <div className="flex flex-1 bg-content-background h-[750px] rounded-xl mt-4 overflow-y-auto">
+            <div className="flex flex-wrap flex-1 justify-center gap-x-[52px] pt-10 pb-8">
+              {collaborators.map((collaborator) => {
+                return (
+                  <Link to={`/profile/${collaborator.id}`}>
+                    <div
+                      key={collaborator.email}
+                      className="w-[256px] h-[310px]"
+                    >
+                      <Card
+                        imageSrc={collaborator.imageSrc}
+                        name={collaborator.name}
+                        role={collaborator.role}
+                        email={collaborator.email}
+                      />
+                    </div>
+                  </Link>
+                );
+              })}
             </div>
-        </div>
+          </div>
+        </main>
+      </div>
+    </div>
   );
 };
 
